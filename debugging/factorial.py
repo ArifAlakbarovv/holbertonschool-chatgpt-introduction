05ee33b1d461752216f2cd8966f3b09550074294
ArifAlakbarovv <116842869+ArifAlakbarovv@users.noreply.github.com>
2024-05-13 17:50:46 +0400

    Update factorial.py

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 import sys
 
-
 def factorial(n):
     result = 1
     while n > 1:
@@ -9,24 +8,5 @@ def factorial(n):
         n = n - 1
     return result
 
-
-def main():
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <number>")
-        sys.exit(1)
-    try:
-        n = int(sys.argv[1])
-    except ValueError:
-        print("Error: Invalid input. Please provide a valid integer.")
-        sys.exit(1)
-
-    if n < 0:
-        print("Error: Factorial is not defined for negative numbers.")
-        sys.exit(1)
-
-    f = factorial(n)
-    print(f)
-
-
-if __name__ == "__main__":
-    main()
+f = factorial(int(sys.argv[1]))
+print(f)
